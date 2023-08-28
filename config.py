23dba46ea75821c6c872d447feb7bed087ff6e38
zhenhinkuan1314 <137288773+zhenhinkuan1314@users.noreply.github.com>
2023-08-28 11:50:16 +0800

    blablablba

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cb2105tbcsnf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3237"
+customdb = "employee"
+custombucket = "kuanzhenhin-employee"
+customregion = "us-east-1"
+
